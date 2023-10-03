717565121c57160ed2c4f63dc80c84efd3885698
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-03 08:25:48 +0100

    headers design set

--- src/components/markdown/markdown.js
+++ src/components/markdown/markdown.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Markdown.scss'
+import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
+import { faMaximize } from '@fortawesome/free-solid-svg-icons';
+
+
 
 
 function Markdown() {
   return ( 
         <div className='markdown'>
-          <div className='markdown-header'>
-            <p>Editor</p>
+          <div className='markdown-header'> 
+            <span className='markdown-title'><FontAwesomeIcon icon={faFreeCodeCamp} /> <strong>Editor</strong></span>
+            <span className='markdown-expand-button'><FontAwesomeIcon icon={faMaximize} /></span>
           </div>
           <div className='markdown-content'>
-            <textarea>eqzdfezdfe</textarea> 
+            <textarea>zzz</textarea> 
           </div>
         </div> 
   )
--- src/components/previewer/previewer.js
+++ src/components/previewer/previewer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './previewer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
+import { faMaximize } from '@fortawesome/free-solid-svg-icons';
 
 function Previewer() {
   return (
     <div className='previewer'>
       <div className='previewer-header'>
-        <p>previewer</p>
+        <span className='previewer-title'><FontAwesomeIcon icon={faFreeCodeCamp} /> <strong>Previewer</strong></span>
+        <span className='previewer-expand-button'><FontAwesomeIcon icon={faMaximize} /></span>
       </div>
       <div className='previewer-content'>
         <p>Lorem Ipsum</p>
