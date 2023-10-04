66dc3dd053b4f48a53031d68329706ac42d92a6f
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-04 10:44:20 +0100

    h1-h2-parag-logique-set

--- src/components/previewer/previewer.js
+++ src/components/previewer/previewer.js
@@ -3,23 +3,43 @@ import './previewer.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
 import { faMaximize } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Previewer() {
   const text = useSelector(state => state.rootreducer.text);
 
-  const printelement = () => {
-    if(text.startsWith("# ")){
-      return <h1>{text}</h1>
+
+
+  const getFormattedElement = (line, index, format) => {
+    switch (format) {
+      case 'h1':
+        return <h1 key={index}>{line}</h1>;
+      case 'h2':
+        return <h2 key={index}>{line}</h2>;
+      case 'h3':
+        return <h3 key={index}>{line}</h3>;
+      default:
+        return <p>{line}</p>
+      
+    }
+  };
+  const formattedText = text.split('\n').map((line, index) => {
+    console.log(line);
+    console.log(index);
+    let currentFormat = '';
+    if(line.startsWith("# ")){
+      currentFormat = 'h1';
     } 
-    if(text.startsWith("## ")){
-      return <h2>{text}</h2>
-    } 
-    else {
-      return <p>{text}</p>
+    else if(line.startsWith("## ")){
+      currentFormat = 'h2';
+    } else if(line.startsWith("### ")){
+      currentFormat = 'h3';
+    } else {
+      currentFormat = '';
     }
     
-  }
+    return getFormattedElement(line, index, currentFormat);
+  });
 
   return (
     <div className='previewer'>
@@ -28,7 +48,7 @@ function Previewer() {
         <span className='previewer-expand-button'><FontAwesomeIcon icon={faMaximize} /></span>
       </div>
       <div className='previewer-content'>
-        {printelement()}
+        {formattedText}
       </div>
     </div>
   )
