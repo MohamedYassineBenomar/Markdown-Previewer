af236f1da18156093e7320e7516d66c99e04630d
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-07 02:41:42 +0100

    add default text

--- src/components/markdown/markdown.js
+++ src/components/markdown/markdown.js
@@ -7,44 +7,28 @@ import { faMaximize } from '@fortawesome/free-solid-svg-icons';
 import { texthandler } from '../../reducers/previewerreducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { defaulttextarea } from '../../reducers/previewerreducer';
+import { useEffect } from 'react'; // Add this line
 
 function Markdown() {
   const dispatch = useDispatch();
   const text = useSelector(state => state.rootreducer.defaulttext);
-  const default_text_textarea = `# Welcome to my React Markdown Previewer!
-## This is a sub-heading...
-### And here's some other cool stuff:
+  useEffect(() => {
+    dispatch(defaulttextarea());
+  }, [dispatch]); 
 
-Heres some code, \`<div></div>\`, between 2 backticks.
-
-\`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-if (firstLine == '\\\`\`\`' && lastLine == '\\\`\`\`') {
-  return multiLineCode;
-  }
-}
-\`\`\`
-
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossingstuffout~~.
-`;
   const handleTextareaChange = (event) => {
     dispatch(texthandler(event.target.value));
    
   };
 
   return ( 
-        <div className='markdown'>
+        <div className='markdown'>  
           <div className='markdown-header'> 
             <span className='markdown-title'><FontAwesomeIcon icon={faFreeCodeCamp} /> <strong>Editor</strong></span>
             <span className='markdown-expand-button'><FontAwesomeIcon icon={faMaximize} /></span>
           </div>
           <div className='markdown-content'>
-            <textarea id='textarea1' onChange={handleTextareaChange} value={default_text_textarea} ></textarea> 
+            <textarea id='textarea1' onChange={handleTextareaChange} >{text}</textarea> 
           </div>
         </div> 
   )
--- src/reducers/previewerreducer.js
+++ src/reducers/previewerreducer.js
@@ -4,51 +4,31 @@ const previewerreducer = createSlice({
     name:"previewer",
     initialState:{
         defaulttext:`# Welcome to my React Markdown Previewer!
-        ## This is a sub-heading...
-        ### And here's some other cool stuff:
+## This is a sub-heading...
+### And here's some other cool stuff:
         
-        Heres some code, \`<div></div>\`, between 2 backticks.
+Heres some code, \`<div></div>\`, between 2 backticks.
         
-        \`\`\`
-        // this is multi-line code:
+\`\`\`
+// this is multi-line code:
         
-        function anotherExample(firstLine, lastLine) {
-        if (firstLine == '\\\`\`\`' && lastLine == '\\\`\`\`') {
-          return multiLineCode;
-          }
+function anotherExample(firstLine, lastLine) {
+if (firstLine == '\\\`\`\`' && lastLine == '\\\`\`\`') {
+        return multiLineCode;
         }
-        \`\`\`
+    }
+\`\`\`
         
-        You can also make text **bold**... whoa!
-        Or _italic_.
-        Or... wait for it... **_both!_**
-        And feel free to go crazy ~~crossingstuffout~~.
-        `,
-        text:`# Welcome to my React Markdown Previewer!
-        ## This is a sub-heading...
-        ### And here's some other cool stuff:
-        
-        Heres some code, \`<div></div>\`, between 2 backticks.
-        
-        \`\`\`
-        // this is multi-line code:
-        
-        function anotherExample(firstLine, lastLine) {
-        if (firstLine == '\\\`\`\`' && lastLine == '\\\`\`\`') {
-          return multiLineCode;
-          }
-        }
-        \`\`\`
-        
-        You can also make text **bold**... whoa!
-        Or _italic_.
-        Or... wait for it... **_both!_**
-        And feel free to go crazy ~~crossingstuffout~~.
-        `
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossingstuffout~~.
+`,
+        text:''
     },
     reducers:{
         defaulttextarea: (state) => {
-            document.querySelector("#root > div > div > div.markdown > div.markdown-content > textarea").value = state.defaulttext
+            state.text = state.defaulttext
         },
         texthandler: (state,action) => {
             state.text = action.payload;
